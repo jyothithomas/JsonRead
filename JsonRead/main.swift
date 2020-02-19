0d28f1fdfc15a0969b99b53377a39d7dfc6f7b3e
jyothithomas <jyothithomas.05@gmail.com>
2020-02-19 18:50:13 -0500

    [jyothi] commit

--- JsonRead/main.swift
+++ JsonRead/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+func readTextFile(fileName: String)
+{
+    print(Bundle.main.bundlePath)
+    if let path = Bundle.main.path(forResource: fileName, ofType: "txt")
+    {
+       // print(path)
+        let str = try! String(contentsOfFile: path)
+        print(str)
+    }
+}
 
+readTextFile(fileName: "data")
+
+func readJSONUserInfo(fileName: String)
+{
+    let filePath = Bundle.main.url(forResource: fileName, withExtension: "json")
+    guard  let path = filePath
+        else {
+        print("Invalid File path")
+        return
+    }
+    guard let data = try? Data(contentsOf: path) else
+    {
+        print("Error while read from URL")
+        return
+    }
+    
+    guard let json = try? JSONSerialization.jsonObject(with: data, options: [])
+        else {
+            print("error")
+            return
+    }
+    print(json)
+}
+
+readJSONUserInfo(fileName: "Singleuser")
