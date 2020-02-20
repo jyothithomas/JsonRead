64848775c8fa318e517445237e34ba4c4096ff90
jyothithomas <jyothithomas.05@gmail.com>
2020-02-19 19:20:06 -0500

    [jyothi] commit

--- JsonRead/main.swift
+++ JsonRead/main.swift
@@ -45,15 +45,38 @@ func readJSONUserInfo(fileName: String)
     if let userDictionary = json as? [String: Any]
     {
         print(userDictionary)
-         let id = userDictionary["id"] ?? "No ID Found"
+        let id = userDictionary["id"] ?? "No ID Found"
             print(id)
+        let name = userDictionary["name"] ?? "Name not Found"
+            print(name)
+        let username = userDictionary["username"] ?? "UserName not Found"
+        print(username)
+        let email = userDictionary["email"] ?? "email not Found"
+        print(email)
         if let addressDictionary = userDictionary["address"] as? [String: Any]
         {
+            print(addressDictionary["street"]!)
+            print(addressDictionary["suite"]!)
             print(addressDictionary["city"]!)
+            print(addressDictionary["zipcode"]!)
+            if let geoDictionary = addressDictionary["geo"] as? [String: Any]
+            {
+                print(geoDictionary["lat"]!)
+                print(geoDictionary["lng"]!)
+            }
         }
-        
-        
+        let phone = userDictionary["phone"] ?? "phone number not Found"
+        print(phone)
+        let website = userDictionary["website"] ?? "website not Found"
+        print(website)
+        if let companyDictionary = userDictionary["company"] as? [String: Any]
+        {
+            print(companyDictionary["name"]!)
+            print(companyDictionary["catchPhrase"]!)
+            print(companyDictionary["bs"]!)
+    
+        }
     }
 }
+    readJSONUserInfo(fileName: "Singleuser")
 
-readJSONUserInfo(fileName: "Singleuser")
